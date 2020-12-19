eb3b8e33472d7651a094d2ac604ef1f5141907d1
贾轩 <15702979536@163.com>
2020-12-19 23:10:01 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2dc11a0743dcbd5d042d953c3345052'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '46123120-d688-477d-909a-81a206eac374'
+};
